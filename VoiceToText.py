9fa32a43d7c78c8e2693959e7afa97107795e4ee
Zyagaimo <yusaku.okada0932@gmail.com>
2023-06-02 01:39:33 +0900

    commit

--- VoiceToText.py
+++ VoiceToText.py
@@ -1,35 +1,32 @@
-# pyaudioというライブラリを使ってwavファイルを生成する
 import pyaudio
 import wave
-# バッファサイズ（録音の遅延）
+ 
 CHUNK = 2**10
 FORMAT = pyaudio.paInt16
-# 使用するマイクの本数
 CHANNELS = 1
 RATE = 44100
-record_time = 1
-output_path = "/home/user/Documents/hogehoge/rec.wav"
-
-# インスタンス生成
+record_time = 5
+output_path = "./output.wav"
+ 
 p = pyaudio.PyAudio()
 stream = p.open(format=FORMAT,
                 channels=CHANNELS,
                 rate=RATE,
                 input=True,
                 frames_per_buffer=CHUNK)
-
-print("録音を開始します...")
-# ここにバッファごとに録音された信号を追加していく
+ 
+print("Recording ...")
 frames = []
-for i in range(0,int(RATE/CHUNK * record_time)):
+for i in range(0, int(RATE / CHUNK * record_time)):
     data = stream.read(CHUNK)
     frames.append(data)
-print("録音終了")
+print("Done.")
+ 
 stream.stop_stream()
 stream.close()
-p.terminate()    
-    
-wf = wave.open(output_path,'wb')
+p.terminate()
+ 
+wf = wave.open(output_path, 'wb')
 wf.setnchannels(CHANNELS)
 wf.setsampwidth(p.get_sample_size(FORMAT))
 wf.setframerate(RATE)
